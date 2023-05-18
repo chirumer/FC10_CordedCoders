ed1ccbc4a9ca592d3609be2dac4356f503250a2d
chirumer <imchirumer@gmail.com>
2023-05-18 12:27:02 +0530

    surpress warnings

--- testing.py
+++
@@ -1,7 +0,0 @@
-from transformers import AlbertTokenizer, AlbertModel
-tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
-model = AlbertModel.from_pretrained("albert-base-v2")
-text = "Replace me by any text you'd like."
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
-print(output)
