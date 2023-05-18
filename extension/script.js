2027c3a3f14d6d0987c8674e85bf46f5f8ae8268
chirumer <imchirumer@gmail.com>
2023-05-18 22:53:33 +0530

    menu injection

--- extension/script.js
+++ extension/script.js
@@ -1,1 +1,11 @@
-alert('hi');
+function inject_menu() {
+  const menu = document.createElement('div');
+  menu.setAttribute('id', 'LearnMate_menu');
+  img_url = chrome.runtime.getURL('menu_image.gif');
+  menu.innerHTML = `
+    <img id="LearnMate_img" src="${img_url}" />
+  `;
+  document.body.appendChild(menu);
+}
+
+inject_menu();
