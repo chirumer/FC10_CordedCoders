2b1f0f142fbe293b3e095ba42e87be07d1fa35a7
chirumer <imchirumer@gmail.com>
2023-05-18 21:01:30 +0530

    general purpose parsing for any article site

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
 from flask_cors import CORS
 from urllib.parse import urlparse
-
-from wikipedia_questions import questions_from_wiki_page
+from question_generation import generate_questions
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 app = Flask(__name__)
 CORS(app)
@@ -12,18 +13,17 @@ CORS(app)
 def hello():
     return 'server active'
 
-f = open('notebooks/sample.txt', 'r')
-t = f.read()
-
 @app.route('/generate_questions')
 def generate_questions_route():
 
-  url = urlparse(request.args.get("url"))
+  url = request.args.get("url")
 
-  if url.hostname.endswith('wikipedia.org'):
-    return questions_from_wiki_page(url)
+  print(url)
+  html = urlopen(url) 
+  soup = BeautifulSoup(html, 'html.parser')
+  text = ''.join([i.getText() for i in soup.findAll('p')])
 
-  return 'unsupported'
+  return generate_questions(text[:10240])
 
 
 if __name__ == '__main__':
